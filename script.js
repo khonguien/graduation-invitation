4c7f18f9f011766b17630c0d5d9a7f8a18e7e772
khonguien <jojomaiden2@gmail.com>
2026-05-05 09:45:30 +0800

    upd

--- script.js
+++ script.js
@@ -24,29 +24,3 @@ document.querySelectorAll('.fade-in').forEach(el => {
     observer.observe(el);
 });
 
-// window.addEventListener('load', () => {
-//     const duration = 3000;
-//     const end = Date.now() + duration;
-
-//     (function frame() {
-//         confetti({
-//             particleCount: 5,
-//             angle: 60,
-//             spread: 55,
-//             origin: { x: 0 },
-//             colors: ['#d4af37', '#2c2c2c', '#ffffff']
-//         });
-        
-//         confetti({
-//             particleCount: 5,
-//             angle: 120,
-//             spread: 55,
-//             origin: { x: 1 },
-//             colors: ['#d4af37', '#2c2c2c', '#ffffff']
-//         });
-
-//         if (Date.now() < end) {
-//             requestAnimationFrame(frame);
-//         }
-//     }());
-// });
